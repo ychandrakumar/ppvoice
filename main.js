78e9db8d10b83ae10ad6d8d74c31eed5133dd7ab
ychandrakumar <ydchandrakumar@gmail.com>
2024-12-09 21:33:38 +0530

    Update main.js
    removed audo (my one debuging

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ const hangupButton = document.getElementById('hangupButton');
 
 // 1. Setup media sources
 webcamButton.onclick = async () => {
-  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  localStream = await navigator.mediaDevices.getUserMedia({ audio: true });
   remoteStream = new MediaStream();
 
   // Push tracks from local stream to peer connection
